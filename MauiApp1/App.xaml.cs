dbf6e753ff185951b87fa640214cb3aa6c4103af
Marcos Gomes <129970704+marcosgxmes@users.noreply.github.com>
2025-03-16 22:10:34 -0300

    implementação da searchbar

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -1,7 +1,27 @@
-﻿namespace MauiApp1
+﻿using MauiApp1.Helpers;
+
+namespace MauiApp1
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    "Banco_sqlite_compras.db3");
+                        
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
--- MauiApp1/Views/ListaProduto.xaml.cs
+++ MauiApp1/Views/ListaProduto.xaml.cs
@@ -1,9 +1,59 @@
+using MauiApp1.Models;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
 namespace MauiApp1.Views;
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = lista;
 	}
+
+	protected async override void  OnAppearing()
+	{
+		List<Produto> tmp = await App.Db.GetAll();
+
+		tmp.ForEach(i => lista.Add(i));
+	}
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PushAsync(new Views.NovoProduto());
+		} catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string q = e.NewTextValue;
+
+        lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i => i.Total);
+
+		string msg = $"O total é {soma:C}";
+
+		DisplayAlert("Total do produtos", msg, "OK");
+    }
+
+    private void MenuItem_ChildAdded(object sender, ElementEventArgs e)
+    {
+
+    }
 }
