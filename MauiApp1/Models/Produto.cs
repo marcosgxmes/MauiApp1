fed2a27b85a09904aa442e90b3ef011f457f3c32
Marcos Gomes <129970704+marcosgxmes@users.noreply.github.com>
2025-03-22 19:36:45 -0300

    implementações de avisos sobre campos vazios

--- MauiApp1/Models/Produto.cs
+++ MauiApp1/Models/Produto.cs
@@ -4,11 +4,48 @@ namespace MauiApp1.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade;
+        double _preco;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set;  }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public string Descricao {
+            get => _descricao;
+            set
+            {
+                if(value == null) 
+                {
+                    throw new Exception("O campo descrição é obrigatório!");
+                }
+
+                _descricao = value;
+            }
+        }
+        public double Quantidade {
+            get => _quantidade;
+            set
+            {
+                if(value == 0)
+                {
+                    throw new Exception("Por favor, insira uma quantidade válida!");
+                }
+
+                _quantidade = value;
+            } 
+        }
+        public double Preco {
+            get => _preco;
+            set
+            {
+                if(value == 0)
+                {
+                    throw new Exception("Por favor, insira um preço válido!");
+                }
+
+                _preco = value;
+            }
+        }
         public double Total { get => Quantidade * Preco; }
     }
 }
